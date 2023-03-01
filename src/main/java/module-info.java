7dd91cb091a444221e3d0bcfd74cec3648bbe7ae
Fahad <fahadfaruqi1@gmail.com>
2023-03-01 15:51:11 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.mycolor2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.mycolor2 to javafx.fxml;
+    exports com.example.mycolor2;
+}
